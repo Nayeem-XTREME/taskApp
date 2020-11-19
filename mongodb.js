19fd33b8c4fdf333e53017310cbd0a4e3429ab09
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-11-20 00:46:33 +0600

    printing inserted documet

--- mongodb.js
+++ mongodb.js
@@ -5,14 +5,20 @@ const mongoClient = mongodb.MongoClient;
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const dbName = 'taskApp'
 
-mongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+mongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         console.log('Unable to connect to database');
     } else {
         const db = client.db(dbName);
         db.collection('users').insertOne({
-            name: 'Niaz Morshed',
+            name: 'Arman Asif',
             age: 25
+        }, (error, result) => {
+            if (error) {
+                console.log('Unable to insert user');
+            } else {
+                console.log(result.ops);
+            }
         })
     }
 })
