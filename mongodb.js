0d8097a343b1c6acfb617965e520af705cc47b9b
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-11-20 00:16:30 +0600

    Test insertion

--- mongodb.js
+++ mongodb.js
@@ -9,6 +9,10 @@ mongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     if (error) {
         console.log('Unable to connect to database');
     } else {
-        console.log('Connected to Mongodb');
+        const db = client.db(dbName);
+        db.collection('users').insertOne({
+            name: 'Niaz Morshed',
+            age: 25
+        })
     }
 })
