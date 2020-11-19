ab4d8af00736440f72020fab9754ac0638070e18
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-11-20 01:02:09 +0600

    Insert Many

--- mongodb.js
+++ mongodb.js
@@ -10,12 +10,33 @@ mongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
         console.log('Unable to connect to database');
     } else {
         const db = client.db(dbName);
-        db.collection('users').insertOne({
-            name: 'Arman Asif',
-            age: 25
-        }, (error, result) => {
+        // db.collection('users').insertOne({
+        //     name: 'Arman Asif',
+        //     age: 25
+        // }, (error, result) => {
+        //     if (error) {
+        //         console.log('Unable to insert user');
+        //     } else {
+        //         console.log(result.ops);
+        //     }
+        // })
+
+        db.collection('users').insertMany([
+            {
+                name: 'Nazmul Huda',
+                age: 26
+            }, 
+            {
+                name: 'Reyad Rahman',
+                age: 27
+            },
+            {
+                name: 'Shahriar Mahmud',
+                age: 25
+            }
+        ], (error, result) => {
             if (error) {
-                console.log('Unable to insert user');
+                console.log('Failed to insert many');
             } else {
                 console.log(result.ops);
             }
