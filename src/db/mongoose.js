ed6d2eb4490d63be7e961e6825ba6d31b4c1f863
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-11-21 01:10:03 +0600

    Validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 // Same as mongodb, but only the database name is appended after the url -> task-manager-api
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
@@ -7,37 +8,37 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { useNewUrlParser
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true
     }, 
     age: {
-        type: Number
-    }
-})
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive')
+            }
+        }
     },
-    completed: {
-        type: Boolean
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email address!');
+            }
+        }
     }
 })
 
 // Create a new user
 const me = new User({
-    name: 'Niaz Morshed',
-    age: 25
+    name: 'Nayeem',
+    email: 'nayeem@gMail.com '
 })
 
 // Save into database
 me.save()
 .then(() => console.log(me))
 .catch(err => console.log(err))
-
-const task = new Task({
-    description: 'Cleaning garder',
-    completed: false
-})
-
-task.save()
-.then(() => console.log(task))
-.catch(err => console.log(err));
