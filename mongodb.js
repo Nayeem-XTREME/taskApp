a517e688a0a5f17d22368faf8ec22ca2cf297d04
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-11-20 10:18:41 +0600

    Find from MongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,45 +1,44 @@
 // CRUD 
-const mongodb = require('mongodb');
-const mongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+// Deconstructing
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const dbName = 'taskApp'
 
-mongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         console.log('Unable to connect to database');
     } else {
         const db = client.db(dbName);
-        // db.collection('users').insertOne({
-        //     name: 'Arman Asif',
-        //     age: 25
-        // }, (error, result) => {
+        
+        // db.collection('users').findOne({_id: new ObjectID('5fb6be958740b11ca890cb42')}, (error, result) => {
         //     if (error) {
-        //         console.log('Unable to insert user');
+        //         console.log('Unable to fetch');
         //     } else {
-        //         console.log(result.ops);
+        //         console.log(result);
         //     }
         // })
 
-        db.collection('users').insertMany([
-            {
-                name: 'Nazmul Huda',
-                age: 26
-            }, 
-            {
-                name: 'Reyad Rahman',
-                age: 27
-            },
-            {
-                name: 'Shahriar Mahmud',
-                age: 25
-            }
-        ], (error, result) => {
-            if (error) {
-                console.log('Failed to insert many');
+        // 'find' returns a 'Cursor'
+        db.collection('users').find({ age: 25 }).toArray((err, res) => {
+            if (err) {
+                console.log('Unable to fetch');
             } else {
-                console.log(result.ops);
+                console.log(res);
             }
         })
+
+        db.collection('users').find({ age: 25 }).count((err, res) => {
+            if (err) {
+                console.log('Unable to fetch');
+            } else {
+                console.log(res);
+            }
+        })
+
     }
 })
